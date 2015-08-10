16292cc4a57376b6358a1295affaed0d96fb2de9
Rochelly <rochelly@rochelly-pc.dti-dev.ufvjm>
2015-08-10 09:43:20 -0300

    Modificação de boletim  para  funcionarios de carga hoŕaria de 4 ou 6 horas

--- api.js
+++ api.js
@@ -7,6 +7,7 @@
     var domain = require('domain');
     var d = domain.create();
     var fs = require('fs');
+
     function encrypt(obj) {
         if (obj == null) obj = [];
         var cipher = crypto.createCipheriv(config.api.cipher, config.api.key, config.api.iv);
@@ -14,6 +15,7 @@
         crypted += cipher.final('hex');
         return crypted;
     }
+
     router.get('/servidor/:id', function(req, res) {
         pontodb.servidor(req.params.id, function(err, rows) {
             if (err) {
@@ -37,6 +39,7 @@
             });
         });
     });
+
     router.get('/feriados/:mes/:ano', function(req, res) {
         pontodb.feriados(req.params.mes, req.params.ano, function(err, rows) {
             if (err) {
@@ -47,6 +50,7 @@
             });
         });
     });
+
     router.get('/ultima_atualizacao', function(req, res) {
         pontodb.ultima_atualizacao(function(err, rows) {
             if (err) {
@@ -59,6 +63,7 @@
             });
         });
     });
+
     router.get('/legendas/:id/:mes/:ano', function(req, res) {
         pontodb.legendas(req.params.id, req.params.mes, req.params.ano, function(err, rows) {
             if (err) {
@@ -69,6 +74,7 @@
             });
         });
     });
+
     router.get('/departamento/:id/:mes/:ano', function(req, res) {
         pontodb.departamento(req.params.id, req.params.mes, req.params.ano, function(err, rows) {
             if (err) {
@@ -79,6 +85,7 @@
             });
         });
     });
+
     //-------------------------Todos os Departamentos---------------------------------------------------------------------------
     router.get('/departamentosAll/', function(req, res) {
         pontodb.departamentosAll(function(err, rows) {
@@ -90,6 +97,7 @@
             });
         });
     });
+
     //-------------------------Horarios dos servidor----------------------------------------------------------------------------
     router.get('/horarios/:siape', function(req, res) {
         pontodb.horarios(req.params.siape, function(err, rows) {
@@ -101,6 +109,7 @@
             });
         });
     });
+
     //-------------------------Verifica a Existencia de terceira entrada e retorna a quantidade --------------------------------
     router.get('/terceiraentrada/:id/:mes/:ano', function(req, res) {
         pontodb.terceiraEntrada(req.params.id, req.params.mes, req.params.ano, function(err, rows) {
@@ -112,6 +121,7 @@
             });
         });
     });
+
     //--------------------------Verifica se e um chefe--------------------------------------------------------------------------
     router.get('/chefia/:usuario', function(req, res) {
         pontodb.chefia(req.params.usuario, function(err, rows) {
@@ -123,6 +133,7 @@
             });
         });
     });
+
     //--------------------------Dados do Chefe----------------------------------------------------------------------------------
     router.get('/chefiaDados/:usuario', function(req, res) {
         pontodb.chefiaDados(req.params.usuario, function(err, rows) {
@@ -134,6 +145,7 @@
             });
         });
     });
+
     //--------------------------Lista de Funcionarios por Departamento----------------------------------------------------------
     router.get('/funcionariosDep/:Departamento', function(req, res) {
         pontodb.funcionariosDep(req.params.Departamento, function(err, rows) {
@@ -147,6 +159,7 @@
             }
         });
     });
+
     //--------------------------Ocorrecias Por Setor e  por  pessoa ------------------------------------------------------------
     router.get('/ocorrencias/:mes/:ano/:departamento/:siape', function(req, res) {
         pontodb.ocorrencias(req.params.mes, req.params.ano, req.params.departamento, req.params.siape, function(err, rows) {
@@ -160,6 +173,7 @@
             }
         });
     });
+
     //--------------------------Dias Trabalhandos e Nao  trabalhos por pessoa --------------------------------------------------
     router.get('/diasTrabalhados/:mes/:ano/:departamento/:siape', function(req, res) {
         pontodb.diasTrabalhados(req.params.mes, req.params.ano, req.params.departamento, req.params.siape, function(err, rows) {
@@ -173,6 +187,7 @@
             }
         });
     });
+
     //--------------------------Dias de Ocorrecias por  pessoa -----------------------------------------------------------------
     router.get('/diasOcorrencias/:mes/:ano/:departamento/:siape/:ocorrencia', function(req, res) {
         pontodb.diasOcorrencias(req.params.mes, req.params.ano, req.params.departamento, req.params.siape, req.params.ocorrencia, function(err, rows) {
@@ -186,6 +201,7 @@
             }
         });
     });
+
     //--------------------------Verifica se o Servidor é estudante--------------------------------------------------------------
     router.get('/estudanteBool/:siape/:pergunta_id', function(req, res) {
         pontodb.estudanteBool(req.params.siape, req.params.pergunta_id, function(err, rows) {
@@ -215,7 +231,8 @@
             }
         });
     });
-    //----------------------Tratamento de Erros----------------------------------------------------------------------------------------------------
+
+    //----------------------Tratamento de Erros---------------------------------------------------------------------------------
     d.run(function() {
         app.use('/api', router);
         pontodb.connect(function(err) {
@@ -228,6 +245,7 @@
             console.log('Listening on', config.api.host + ':' + config.api.port);
         });
     });
+
     d.on('error', function(err) {
         console.error('CAPTURA :', err);
         if (err) {
